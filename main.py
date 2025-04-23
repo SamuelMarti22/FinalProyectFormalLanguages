4528782155211ea87ea7fb7c7df3024fe32b622c
SamuelMarti22 <samuelmartinezarteaga22@gmail.com>
2025-04-23 18:36:35 -0500

    Improve parsing functions in main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Rule import Rule
+import pandas as pd
 
 
 def print_rules(rules):
@@ -26,7 +27,7 @@ def first(production,diccFirst,flagLL):
     simbolo = production.get_produccion()[0]
 
     if simbolo == production.get_simbolo_produccion():
-        print("Reduccion por la izquieda")
+        print("Recursion por la izquieda")
         flagLL = False
         print("Flag LL: ", flagLL)
         production.set_first(".")
@@ -35,7 +36,8 @@ def first(production,diccFirst,flagLL):
             production.set_first(simbolo)
             if simbolo not in diccFirst[production.get_simbolo_produccion()]:
                 diccFirst[production.get_simbolo_produccion()].append(simbolo)
-
+            else:
+                flagLL = False
         else:
             print("Simbolo no terminal")
             production.set_first(diccFirst[simbolo])
@@ -49,13 +51,16 @@ def follow(diccFollow,noTerminal):
         if noTerminal in reglas[i].get_produccion():
             posicion = reglas[i].get_produccion().index(noTerminal)
             if posicion == len(reglas[i].get_produccion())-1:
-                diccFollow[noTerminal].extend(diccFollow[reglas[i].get_simbolo_produccion()])
+                if diccFollow[reglas[i].get_simbolo_produccion()] != [] and diccFollow[reglas[i].get_simbolo_produccion()] != ["$"]:
+                    diccFollow[noTerminal].extend(diccFollow[reglas[i].get_simbolo_produccion()])
+                else:
+                    diccFollow[noTerminal].extend(follow(diccFollow, reglas[i].get_simbolo_produccion()))
                 diccFollow[noTerminal] = list(set(diccFollow[noTerminal]))
             elif reglas[i].get_produccion()[posicion+1] in terminales:
                 diccFollow[noTerminal].append(reglas[i].get_produccion()[posicion+1])
                 diccFollow[noTerminal] = list(set(diccFollow[noTerminal]))
             else:
-                firstNext = diccFirst[reglas[i].get_produccion()[posicion+1]]
+                firstNext = diccFirst[reglas[i].get_produccion()[posicion+1]].copy()
                 if "e" in firstNext:
                     firstNext.remove("e")
                     if diccFollow[reglas[i].get_produccion()[posicion+1]] != []:
@@ -66,12 +71,32 @@ def follow(diccFollow,noTerminal):
                 diccFollow[noTerminal] = list(set(diccFollow[noTerminal]))
     return diccFollow[noTerminal]
 
+def construccionTablaLL(diccFirst, diccFollow,regla):
+    # Implementación de la función para construir la tabla LL(1)
+    if "e" in regla.get_first():
+        for i in range(len(diccFollow[regla.get_simbolo_produccion()])):
+            parsingTableLL[(regla.get_simbolo_produccion(), diccFollow[regla.get_simbolo_produccion()][i])] = regla.get_produccion()
+    else:
+        for i in range(len(regla.get_first())):
+            parsingTableLL[(regla.get_simbolo_produccion(), regla.get_first()[i])] = regla.get_produccion()
 
+def print_parsing_table(parsingTableLL):
+    # Filas y columnas
+    filas = sorted({k[0] for k in parsingTableLL})
+    columnas = sorted({k[1] for k in parsingTableLL})
 
+    # Crear DataFrame
+    df = pd.DataFrame('', index=filas, columns=columnas)
+    for (fila, col), valor in parsingTableLL.items():
+        df.at[fila, col] = valor
 
+    # Reemplazar celdas vacías con '-'
+    df.replace('', '-', inplace=True)
 
-
-
+    # Imprimir con líneas
+    print(df.to_markdown(tablefmt="grid"))
+    
+    
 
 reglas = []
 strings = []
@@ -79,6 +104,7 @@ terminales = []
 noTerminales = []
 flagLL= True
 flagSLR= True
+parsingTableLL = {}
 
 
 
@@ -103,16 +129,17 @@ with open("input.txt", "r") as archivo:
             primero = partes[0]
 
         # Crear una regla para cada producción
-        for i in range(len(partes[1])):
-            identify_terminal(partes[1][i])
-            rule = Rule(partes[0], partes[1][i])
+        for j in range(len(partes[1])):
+            identify_terminal(partes[0])
+            identify_terminal(partes[1][j])
+            rule = Rule(partes[0], partes[1][j])
             reglas.append(rule)
 
 
     # 3. Leer los strings hasta encontrar "e"
     for linea in archivo:
         linea = linea.strip()+"$"
-        if linea == "e":
+        if linea == "e$":
             break
         strings.append(linea)
 
@@ -138,23 +165,32 @@ for i in range (len(reglas)-1,-1,-1):
     print()
 
 
-if(not flagLL):
-    print("La gramatica no es LL(1)")
-#Calcular los follow
-
 for simbolo in noTerminales:
     if diccFollow[simbolo] == [] or diccFollow[simbolo] == ["$"]:
-        #follow(diccFollow, simbolo)
+        follow(diccFollow, simbolo)
         print()
     print(f"Simbolo de produccion: {simbolo}")
     print(f"First: {diccFirst[simbolo]}")
     print(f"Follow: {diccFollow[simbolo]}")
     print()
 
+#Revisar LL(1)
+if not flagLL:
+    print("No es LL(1)")
+else:
+    for i in noTerminales:
+        if "e" in diccFirst[i] and flagLL:
+            for j in range(len(reglas)):
+                if reglas[j].get_simbolo_produccion() == i:
+                    if reglas[j].get_first() in diccFollow[i]:
+                        flagLL= False
+                        print("Ambiguedad")
+                        break
 
-
-
-
+if flagLL:
+    print("Construir table")
+    for i in range(len(reglas)):
+        construccionTablaLL(diccFirst,diccFollow,reglas[i])
 print("Terminales:")
 print(terminales)
 print("No terminales:")
@@ -163,5 +199,5 @@ print("Diccionario FIRST:")
 print(diccFirst)
 print("Diccionario FOLLOW:")
 print(diccFollow)
-print("Flag LL:")
-print(flagLL)
+print("Tabla LL(1):")
+print_parsing_table(parsingTableLL)
